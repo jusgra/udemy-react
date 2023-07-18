6eb4d757d1261475f9e1e9042cd52534ac4f596e
Justas Gražulevičius <91288874+jusgra@users.noreply.github.com>
2023-07-18 23:52:01 +0300

    CSS modules, IMO cleaner way to make the css classes unique, than styledComponents package

--- styling-components/src/components/CourseInput.jsx
+++ styling-components/src/components/CourseInput.jsx
@@ -1,33 +1,6 @@
 import { useState } from "react";
 import Button from "./Button";
-import styled from "styled-components";
-
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? "red" : "black")};
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.invalid ? "red" : "#ccc")};
-    background-color: ${(props) => props.invalid && "#ffd5d5"};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+import styles from "./css/CourseInput.module.css";
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
@@ -48,13 +21,13 @@ const CourseInput = (props) => {
     setEnteredValue("");
     props.onAddGoal(enteredValue);
   };
-
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isInputValid}>
+      {/* <div className={styles["form-control"] + " " + (!isInputValid && styles.invalid)}> */}
+      <div className={`${styles["form-control"]} ${!isInputValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input value={enteredValue} type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
