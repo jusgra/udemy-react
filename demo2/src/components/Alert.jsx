877db3957a1105e33cc05342b3e3f82e6f736d6c
Justas Gražulevičius <91288874+jusgra@users.noreply.github.com>
2023-07-20 23:51:46 +0300

    some code changes

--- demo2/src/components/Alert.jsx
+++ demo2/src/components/Alert.jsx
@@ -1,10 +1,11 @@
 import Card from "./Card";
+import styles from "../css/alert.module.css";
 
 export default function Alert(props) {
   return (
     <div>
-      <div className="alert-background" onClick={props.errorHandle} />
-      <Card className="alert">
+      <div className={styles["alert-background"]} onClick={props.errorHandle} />
+      <Card className={styles.alert}>
         <header>{props.error}</header>
         <p>{props.message}</p>
         <button onClick={props.errorHandle}>okay</button>
