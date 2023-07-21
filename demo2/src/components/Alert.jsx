8283f50c7df1e820b8de4384d299c05d7161a2d8
Justas Gražulevičius <91288874+jusgra@users.noreply.github.com>
2023-07-21 17:02:31 +0300

    implemented protal feature

--- demo2/src/components/Alert.jsx
+++ demo2/src/components/Alert.jsx
@@ -1,15 +1,26 @@
 import Card from "./Card";
 import styles from "../css/alert.module.css";
+import { createPortal } from "react-dom";
+
+const Background = (props) => {
+  return <div className={styles["alert-background"]} onClick={props.errorHandle} />;
+};
+
+const AlertPop = (props) => {
+  return (
+    <Card className={styles.alert}>
+      <header>{props.error}</header>
+      <p>{props.message}</p>
+      <button onClick={props.errorHandle}>okay</button>
+    </Card>
+  );
+};
 
 export default function Alert(props) {
   return (
-    <div>
-      <div className={styles["alert-background"]} onClick={props.errorHandle} />
-      <Card className={styles.alert}>
-        <header>{props.error}</header>
-        <p>{props.message}</p>
-        <button onClick={props.errorHandle}>okay</button>
-      </Card>
-    </div>
+    <>
+      {createPortal(<Background errorHandle={props.errorHandle} />, document.body)}
+      {createPortal(<AlertPop error={props.error} message={props.message} errorHandle={props.message} />, document.body)}
+    </>
   );
 }
